3a7c5021c9a20504d069f8f1a35451ecdb7a7816
Orestis Papadopoulos <orjpap@gmail.com>
2022-02-11 13:29:37 +0200

    Changed twitter handler in info

--- deploy.sh
+++ deploy.sh
@@ -5,11 +5,11 @@ if [[ -z "$1" ]]; then
   exit
 fi
 
-bundle exec jekyll build && \
-  cd _site && \
-  git checkout -f master
-  git add . && \
-  git commit -am "$1" && \
-  git push origin master && \
-  cd .. && \
-  echo "Successfully built and pushed to GitHub."
+bundle exec jekyll build
+cd _site
+git checkout -f master
+git add .
+git commit -am "$1"
+git push origin master
+cd ..
+echo "Successfully built and pushed to GitHub."
